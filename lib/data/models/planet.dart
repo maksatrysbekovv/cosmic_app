c3f4d1b660c728831807a053b285155320503c9e
maksatrysbekovv <maksatrysbekov99@gmail.com>
2025-10-23 18:20:15 +0600

    Коммить

---
+++ lib/data/models/planet.dart
@@ -0,0 +1,11 @@
+class Planet {
+  final String name;
+  final String description;
+  final String image;
+
+  Planet({
+    required this.name,
+    required this.description,
+    required this.image,
+  });
+}
--- lib/ui/screens/home/home_page.dart
+++ lib/ui/screens/home/home_page.dart
@@ -7,14 +7,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: const Text('Home Page', style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.black,
-      ),
+      backgroundColor: Colors.red.withOpacity(0.2),
+      // appBar: AppBar(
+      //   title: const Text(
+      //     'Solar Sytemdds',
+      //     style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      //   ),
+      //   backgroundColor: Colors.black.withOpacity(0.1),
+      // ),
       body: Stack(
         children: [
           Background(),
+          AppBar(
+            title: const Text(
+              'Solar Sytemds',
+
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            backgroundColor: Colors.black.withOpacity(0.1),
+          ),
           Center(
             child: Text(
               'Welcome to the Home Page',
